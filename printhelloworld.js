5bd0dbfb7a67c4032141f49c39080a282b16c12e
eabsystemswilson <101030841+eabsystemswilson@users.noreply.github.com>
2022-07-23 10:14:11 +0800

    commit

--- printhelloworld.js
+++ printhelloworld.js
@@ -3,6 +3,10 @@ myHeading.textContent = 'Hello world! H1';
 
 function printHelloWorld() {
   console.log('printHelloWorld is called');
+  console.log('try to call testFunction');
+  testFunction();
+  console.log('testFunction called');
+  
   return "Hello World";
 }
 
