4bfc519e5cacabad14e4ea297b4fb6bb26d97aa3
eabsystemswilson <101030841+eabsystemswilson@users.noreply.github.com>
2022-07-23 10:02:54 +0800

    commit

--- printhelloworld.js
+++ printhelloworld.js
@@ -1,4 +1,7 @@
-export function printHelloWorld() {
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world! H1';
+
+function printHelloWorld() {
   console.log('printHelloWorld is called');
   return "Hello World";
 }
